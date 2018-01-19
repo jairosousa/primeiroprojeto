59366d040636c7eb98bea87c65f2d751a039c284
jaironsousa1 <jaironsousa@gmail.com>
2018-01-19 17:51:12 -0200

    Configurando o Profile

--- src/main/java/com/jairosousa/primeiroprojeto/PrimeiroprojetoApplication.java
+++ src/main/java/com/jairosousa/primeiroprojeto/PrimeiroprojetoApplication.java
@@ -1,12 +1,25 @@
 package com.jairosousa.primeiroprojeto;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PrimeiroprojetoApplication {
 
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtdPorPagina;
+
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeiroprojetoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("### Quantidade de Elementos por pagina = " + this.qtdPorPagina);
+		};
 	}
 }
--- src/test/java/com/jairosousa/primeiroprojeto/PrimeiroprojetoApplicationTests.java
+++ src/test/java/com/jairosousa/primeiroprojeto/PrimeiroprojetoApplicationTests.java
@@ -1,16 +1,28 @@
 package com.jairosousa.primeiroprojeto;
 
 import org.junit.Test;
+import org.junit.Assert.*;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class PrimeiroprojetoApplicationTests {
+
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtdPorPagina;
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testCarregarContextoDeTeste() {
+
 	}
 
 }
